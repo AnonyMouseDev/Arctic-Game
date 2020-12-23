30a3abd9217adb8279aa1901701a6ed123220dfb
AnonyMouseDev <75556863+AnonyMouseDev@users.noreply.github.com>
2020-12-22 19:22:45 -0800

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -1,18 +1,51 @@
-﻿using System.Collections;
+using System.Security;
+using System.Net.Cache;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 using UnityEngine;
 
 public class Enemy : Actor
 {
+    Vector2 origin;
+    Vector2 direction;
+
+    RaycastHit2D colliderHit;
+
+    public float radius = 10f; 
+    public float distance = 10f;
+
+    public LayerMask playerLayer;
+
+    public Transform player;
+    public NavMeshAgent agent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        direction = Vector2.zero;
+
+        var agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+    }
+
+    void Update()
+    {
+        origin = transform.position;
+
+        colliderHit = Physics2D.CircleCast(origin, radius, direction, distance, playerLayer);
+        //Debug.Log("Ran it");
+        if (colliderHit.collider == characterCollider)
+        {
+            //Debug.Log("Detected");
+            agent.SetDestination(player.position);
+        }
     }
 
     void FixedUpdate()
     {
-        EnemyAI();
+            
     }
 }
